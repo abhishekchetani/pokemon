cf658b7af2a840819c775697bb8d7bff8168082b
abhishekchetani <abhishekchetani@gmail.com>
2021-08-09 16:38:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.getElementById('container');
+
+// const newImg = document.createElement('img');
+// newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+// container.appendChild(newImg);
+
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`;
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
+
+{/* <div>
+    <img>
+    <span>2</span>
+</div> */}
